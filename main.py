79448d6034e90a4802a1c78c3a75d8c58ae7b0e0
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-01-25 20:40:14 +0200

    refactored code to improve debugging
    You can now debug the code via:
    
        >>> import main
    
    without getting prompted to choose an algorithm.
    
    I also removed the FIXMEs since `y_train` and `y_test` turned out to be
    lists of integer labels (1 or 0) for `x_train` and `x_test`
    respectively.

--- main.py
+++ main.py
@@ -5,46 +5,40 @@ import numpy as np
 import randomforest as rforest
 import tensorflow as tf
 
+# default params to keras.dataset.imdb.load_data
+# see: https://keras.io/api/datasets/imdb
+start_char = 1
+oov_char = 2  # oov = out-of-vocabulary
+index_from = 3
+
+# retrieve training sequences
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
+        start_char=start_char, oov_char=oov_char, index_from=index_from
+ )
+
+# retrieve word index and create it's inverse
+word_to_index = tf.keras.datasets.imdb.get_word_index()
+index_to_word = dict(
+    (i + index_from, word) for (word, i) in word_to_index.items()
+)
+
+# update inverted word index to include start_char and oov_char
+index_to_word[start_char] = '[START]'
+index_to_word[oov_char] = '[OOV]'
+
+# decode each review in the dataset
+x_train = np.array([' '.join([index_to_word[idx] for idx in text])
+                    for text in x_train])
+x_test = np.array([' '.join([index_to_word[idx] for idx in text])
+                   for text in x_test])
+
+# vectorize each review
+binary_vectorizer = CountVectorizer(binary=True, min_df=100)
+x_train_bin = binary_vectorizer.fit_transform(x_train)
+x_test_bin = binary_vectorizer.fit_transform(x_test)
+print('Vocabulary size:', len(binary_vectorizer.vocabulary_))
+
 if __name__ == '__main__':
-    # default params to keras.dataset.imdb.load_data
-    # see: https://keras.io/api/datasets/imdb
-    start_char = 1
-    oov_char = 2  # oov = out-of-vocabulary
-    index_from = 3
-
-    # retrieve training sequences
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
-            start_char=start_char, oov_char=oov_char, index_from=index_from
-     )
-
-    # retrieve word index and create it's inverse
-    word_to_index = tf.keras.datasets.imdb.get_word_index()
-    index_to_word = dict(
-        (i + index_from, word) for (word, i) in word_to_index.items()
-    )
-
-    # update inverted word index to include start_char and oov_char
-    index_to_word[start_char] = '[START]'
-    index_to_word[oov_char] = '[OOV]'
-
-    # decode each review in the dataset
-    x_train = np.array([' '.join([index_to_word[idx] for idx in text])
-                        for text in x_train])
-    x_test = np.array([' '.join([index_to_word[idx] for idx in text])
-                       for text in x_test])
-    y_train = np.array([' '.join([index_to_word[idx] for idx in text])  # FIXME
-                        for text in y_train])
-    y_test = np.array([' '.join([index_to_word[idx] for idx in text])  # FIXME
-                       for text in y_test])
-
-    # TODO build the vocabulary and vectorize each review
-    binary_vectorizer = CountVectorizer(binary=True, min_df=100)
-    x_train_bin = binary_vectorizer.fit_transform(x_train)
-    x_test_bin = binary_vectorizer.fit_transform(x_test)
-    y_train_bin = binary_vectorizer.fit_transform(y_train)  # FIXME
-    y_test_bin = binary_vectorizer.fit_transform(y_test)  # FIXME
-    print('Vocabulary size:', len(binary_vectorizer.vocabulary_))
-
     sel_alg = int(input('Select an algorithm:\n[1]: Naive Bayes \
             \n[2]: Random Forest\n[3]: Adaboost\nYour selection: '))
     if sel_alg > 3 or sel_alg < 1:
