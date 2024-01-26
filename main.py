a7a316618d2aafd599b514d456279f208763748b
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-01-26 19:28:18 +0200

    fixed: binary vectors and vocabulary

--- main.py
+++ main.py
@@ -32,11 +32,16 @@ x_train = np.array([' '.join([index_to_word[idx] for idx in text])
 x_test = np.array([' '.join([index_to_word[idx] for idx in text])
                    for text in x_test])
 
-# vectorize each review
+# vectorize each review and create the vocabulary
 binary_vectorizer = CountVectorizer(binary=True, min_df=100)
-x_train_bin = binary_vectorizer.fit_transform(x_train)
-x_test_bin = binary_vectorizer.fit_transform(x_test)
-print('Vocabulary size:', len(binary_vectorizer.vocabulary_))
+x_train_bin = binary_vectorizer.fit_transform(x_train).toarray()
+x_test_bin = binary_vectorizer.fit_transform(x_test).toarray()
+vocab = np.array(list(binary_vectorizer.vocabulary_.keys()))
+
+# TODO force constraints on vocabulary words
+# 1. contains the m most common,
+# 2. ignores the n most common,
+# 3. ignores the k rarest.
 
 if __name__ == '__main__':
     sel_alg = int(input('Select an algorithm:\n[1]: Naive Bayes \
