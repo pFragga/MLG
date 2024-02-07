a241ce433c0de55baf4e005db2dbeecb6a27abdd
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-02-07 15:27:31 +0200

    added: constraints to vocabulary

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import sys
+
+if len(sys.argv[1:]) != 3:
+    print('Usage:\n\t[python] ./main.py <m> <n> <k>\nwhere m, n and k are '
+          'vocabulary parameters.\n(Hint: try m=1000, n=1000 and k=100)',
+          file=sys.stderr)
+    exit(-1)
+
+_m = int(sys.argv[1])
+_n = int(sys.argv[2])
+_k = int(sys.argv[3])
+
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import randomforest as rforest
@@ -13,11 +25,11 @@ index_from = 3
 
 # retrieve training sequences
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
-        start_char=start_char,
-        oov_char=oov_char,
-        index_from=index_from,
-        num_words=500  # not mandatory, just speeds up debugging
- )
+    start_char=start_char,
+    oov_char=oov_char,
+    index_from=index_from,
+    num_words=5000  # this only speeds up debugging
+)
 
 # retrieve word index and create it's inverse
 word_to_index = tf.keras.datasets.imdb.get_word_index()
@@ -36,20 +48,19 @@ x_test = np.array([' '.join([index_to_word[idx] for idx in text])
                    for text in x_test])
 
 # vectorize each review using CountVectorizer
+# max-df: ignore terms with higher document frequency
 # min-df: ignore terms with lower document frequency
-binary_vectorizer = CountVectorizer(binary=True, min_df=100)
+binary_vectorizer = CountVectorizer(binary=True,
+                                    max_features=_m, max_df=_n, min_df=_k)
 x_train_bin = binary_vectorizer.fit_transform(x_train).toarray()
 x_test_bin = binary_vectorizer.fit_transform(x_test).toarray()
 
 # CountVectorizer produces a dictionary which maps terms to feature indeces
 vocab = np.array(list(binary_vectorizer.vocabulary_.keys()))
+print('Printing vocabulary...', vocab, '\n', len(vocab))
 
-# TODO force constraints on vocabulary
-# 1. contains the m most common words,
-# 2. ignores the n most common,
-# 3. ignores the k rarest.
 
-if __name__ == '__main__':
+def main():
     sel_alg = int(input('Select an algorithm:\n[1]: Naive Bayes \
             \n[2]: Random Forest\n[3]: Adaboost\nYour selection: '))
     if sel_alg > 3 or sel_alg < 1:
@@ -60,10 +71,14 @@ if __name__ == '__main__':
         # exit(2)
     elif sel_alg == 2:
         numberOfTrees = int(input('Number of trees (integer value): '))
-        print(f'Creating a Random Forest with {numberOfTrees} trees...')
-        forest = rforest.RandomForest(numberOfTrees, vocab[:10])  # FIXME
-        forest.train(x_train_bin[:100], y_train[:100])
-        forest.fit(x_test_bin, y_test)
+        print(f'Creating a Random Forest with {numberOfTrees} tree(s)...')
+        forest = rforest.RandomForest(numberOfTrees, vocab)
+        forest.train(x_train_bin, y_train)
+        # forest.fit(x_test_bin, y_test)
     elif sel_alg == 3:
         print('Not yet implemented.')
         # exit(2)
+
+
+if __name__ == '__main__':
+    main()
