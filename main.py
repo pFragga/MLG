6aa7bde51101877d58d570a61de6472227d514e5
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-02-08 11:57:51 +0200

    fixed: import main doesn't crash bc of sys.argv
    Also randomforest.py was executable for some reason...

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-import sys
+# the values of these don't matter when running this script as the main module
+_m = 1000
+_n = 1000
+_k = 100
 
-if len(sys.argv[1:]) != 3:
-    print('Usage:\n\t[python] ./main.py <m> <n> <k>\nwhere m, n and k are '
-          'vocabulary parameters.\n(Hint: try m=1000, n=1000 and k=100)',
-          file=sys.stderr)
-    exit(-1)
+if __name__ == '__main__':
+    import sys
 
-_m = int(sys.argv[1])
-_n = int(sys.argv[2])
-_k = int(sys.argv[3])
+    if len(sys.argv[1:]) != 3:
+        print('Usage:\n\t[python] ./main.py <m> <n> <k>\nwhere m, n and k are'
+              'vocabulary parameters.\n(Hint: try m=1000, n=1000 and k=100)',
+              file=sys.stderr)
+        exit(-1)
+
+    _m = int(sys.argv[1])
+    _n = int(sys.argv[2])
+    _k = int(sys.argv[3])
 
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
@@ -57,10 +63,10 @@ x_test_bin = binary_vectorizer.fit_transform(x_test).toarray()
 
 # CountVectorizer produces a dictionary which maps terms to feature indeces
 vocab = np.array(list(binary_vectorizer.vocabulary_.keys()))
-print('Printing vocabulary...', vocab, '\n', len(vocab))
 
+if __name__ == '__main__':
+    print('Printing vocabulary...', vocab, '\n', len(vocab))
 
-def main():
     sel_alg = int(input('Select an algorithm:\n[1]: Naive Bayes \
             \n[2]: Random Forest\n[3]: Adaboost\nYour selection: '))
     if sel_alg > 3 or sel_alg < 1:
@@ -78,7 +84,3 @@ def main():
     elif sel_alg == 3:
         print('Not yet implemented.')
         # exit(2)
-
-
-if __name__ == '__main__':
-    main()
